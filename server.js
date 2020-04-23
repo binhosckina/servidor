67db6219506bd0663b41c6a02072ef21ac975eaf
binhosckina <binhosckina@gmail.com>
2020-04-22 23:08:44 -0300

    Commit inicial

---
+++ server.js
@@ -0,0 +1,51 @@
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+var cors = require('cors');
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+io.on('connection', (socket) => {
+    socket.on('abrirCaixa', () => {
+        console.log('Caixa aberto');
+        socket.emit('abrirCaixa');
+    });
+
+    socket.on('fecharCaixa', () => {
+        console.log('Caixa fechado');
+        socket.emit('fecharCaixa');
+    });
+
+    socket.on('adicionarProduto', (id, quantidade) => {
+        var produto = produtos.find(x => x.id = id)
+
+        if (produto) {
+            produto.quantidade = quantidade
+            console.log(quantidade + 'x ' + produto.nome + ' adicionado');
+            socket.emit('adicionarProduto', produto);
+        } else {
+            console.log('Produto não encontrado');
+            socket.emit('produtoNaoEncontrado');
+        }
+    });
+});
+
+server.listen(3030, () => {
+    console.log('listening on *:3030')
+})
+
+produtos = [
+    { "id": 1, "nome": "Arroz", "valor": 3 },
+    { "id": 2, "nome": "Feijão", "valor": 4 },
+    { "id": 3, "nome": "Batata", "valor": 5 },
+    { "id": 4, "nome": "Água", "valor": 2 },
+    { "id": 5, "nome": "Carne", "valor": 40 },
+    { "id": 6, "nome": "Cerveja", "valor": 4 },
+    { "id": 7, "nome": "Tomate", "valor": 8 },
+    { "id": 8, "nome": "Cebola", "valor": 1 },
+    { "id": 9, "nome": "Pizza", "valor": 12 }
+]
